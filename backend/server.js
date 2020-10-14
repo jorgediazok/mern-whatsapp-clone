3523e22db50aaf9cf1b59a00217c4df04e09dae6
Jorge <jorgediazok@gmail.com>
2020-10-14 20:11:53 -0300

    db connected to mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
-import express from "express"
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
+
 
 //app config
 const app = express()
 const port = process.env.PORT || 9000
 
 //middleware
+
+
+//DB config
+
+mongoose.connect(
+  process.env.REACT_APP_DB,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('conected to DB');
+  }
+);
 
 
 //api routes
