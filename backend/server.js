d4ec472d87f2a683b22b13777ee97c6ad930f789
Jorge <jorgediazok@gmail.com>
2020-10-14 17:48:22 -0300

    added server on backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+
+//app config
+const app = express()
+const port = process.env.PORT || 9000
+
+//middleware
+
+
+//api routes
+app.get("/", (req,res,next)=>{
+  res.status(200).send("hello world")
+})
+
+//listen
+app.listen(port, ()=>console.log(`Listening on localhost: ${port}`))
