eab1145e4ff291df4f3fddef9f89a969ab78ec26
Jorge <jorgediazok@gmail.com>
2020-10-14 16:38:08 -0300

    styling the body of the chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Avatar} from "@material-ui/core"
+import {Avatar, IconButton} from "@material-ui/core"
 import "../styles/Chat.css"
+import { AttachFile, SearchOutlined, MoreVert } from '@material-ui/icons'
 
 function Chat() {
   return (
@@ -14,9 +15,29 @@ function Chat() {
         <p>Last seen at...</p>  
         </div> 
 
-        <div className="chat__headerRight"></div>
+        <div className="chat__headerRight">
+        <IconButton>
+          <SearchOutlined />
+        </IconButton>
+        <IconButton>
+          <AttachFile/>
+        </IconButton>
+        <IconButton>
+          <MoreVert/>
+        </IconButton>
+        </div>
 
       </div>
+
+      <div className="chat__body">
+          <p className="chat__message">
+           <span className="chat__name">Jorge </span>this is a message             
+           <span className="chat__timestamp">
+             {new Date().toUTCString()}
+           </span>
+          </p>
+        </div>
+
     </div>
   )
 }
