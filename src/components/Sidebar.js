efe08023ec3ec9e0b346b4a8511b942d74de25a9
Jorge <jorgediazok@gmail.com>
2020-10-14 11:41:40 -0300

    top sidebar with icons done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import DonutLargeIcon from "@material-ui/icons/DonutLarge"
+import ChatIcon from "@material-ui/icons/Chat"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+import {IconButton,Avatar} from "@material-ui/core"
 import "../styles/Sidebar.css"
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <div>
-        <h1>i am the sidebar</h1>
+    <div className="sidebar">      
+        <div className="sidebar__header">
+          <Avatar src="https://avatars1.githubusercontent.com/u/47897689?s=460&u=0bb395d093ce35cd806306d0c62b1d86aebfbaa1&v=4" alt="prof"/>
+          
+          <div className="sidebar__headerRight">
+          
+          <IconButton>
+          <DonutLargeIcon />   
+          </IconButton>
+          
+          <IconButton>
+          <ChatIcon/>  
+          </IconButton>
+          
+          <IconButton>
+          <MoreVertIcon /> 
+          </IconButton>   
+
+        </div>
       </div>
     </div>
   )
