74a464a4ef5f281efe40d024cadc9ba9dad74be8
Jorge <jorgediazok@gmail.com>
2020-10-14 13:52:23 -0300

    sidebar done and now working on chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Avatar} from "@material-ui/core"
 import "../styles/Chat.css"
 
 function Chat() {
   return (
     <div className="chat">
-      <h1>i am the chat</h1>
+      <div className="chat__header">
+
+        <Avatar />
+
+        <div className="chat__headerInfo">
+        <h3>Room Name</h3>
+        <p>Last seen at...</p>  
+        </div> 
+
+        <div className="chat__headerRight"></div>
+
+      </div>
     </div>
   )
 }
--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Avatar} from "@material-ui/core"
 import "../styles/SidebarChat.css"
 
 function SidebarChat() {
   return (
-    <div>
-      
+    <div className="sidebarChat">
+     <Avatar />
+     <div className="sidebarChat__info">
+      <h2>Room name</h2>
+      <p>This is the last message</p> 
+      </div> 
     </div>
   )
 }
