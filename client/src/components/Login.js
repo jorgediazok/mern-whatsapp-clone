76848cddec5b37f5f245246ce2c1fffc11b54123
Jorge <jorgediazok@gmail.com>
2020-10-15 03:18:05 -0300

    login with google enabled

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Button} from "@material-ui/core"
+import {auth, provider} from "../firebase"
 import "../styles/Login.css"
 
 function Login() {
 
-  const signIn = () =>{
-
+  const signIn = () => {
+    auth.signInWithPopup(provider).then((result) => console.log(result))
+    .catch(error=>alert(error.message))
   }
 
   return (
