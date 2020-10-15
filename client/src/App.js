70e73733f23ca803c45c0140bc92767ecc8d8cee
Jorge <jorgediazok@gmail.com>
2020-10-14 22:52:33 -0300

    added pusher to the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import Sidebar from "./components/Sidebar"
 import Chat from "./components/Chat"
+import Pusher from "pusher-js"
 import './App.css';
 
 function App() {
+
+useEffect(()=>{
+  const pusher = new Pusher('4b4a0c6249a061ba3cc8', {
+    cluster: 'us2'
+  });
+
+  const channel = pusher.subscribe('messages');
+  channel.bind('inserted', (data) => {
+    alert(JSON.stringify(data));
+  });
+},[])
+
+
   return (
     <div className="app">
       <div className="app__body">
