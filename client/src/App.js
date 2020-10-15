f5157b3db4d3be96eeb36c1117d33fc99883b6af
Jorge <jorgediazok@gmail.com>
2020-10-15 00:04:18 -0300

    app working !

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import Sidebar from "./components/Sidebar"
 import Chat from "./components/Chat"
 import Pusher from "pusher-js"
+import axios from "./axios"
 import './App.css';
 
 function App() {
+
+  const [messages, setMessages] = useState([])
+
+useEffect(()=>{
+  axios.get("/messages/sync").then(response =>{
+    setMessages(response.data)
+  })
+},[])
 
 useEffect(()=>{
   const pusher = new Pusher('4b4a0c6249a061ba3cc8', {
@@ -12,17 +21,23 @@ useEffect(()=>{
   });
 
   const channel = pusher.subscribe('messages');
-  channel.bind('inserted', (data) => {
-    alert(JSON.stringify(data));
+  channel.bind('inserted', (newMessage) => {
+    setMessages([...messages, newMessage])
   });
-},[])
+
+ return () => {
+    channel.unbind_all();
+    channel.unsubscribe();  }
+
+
+},[messages])
 
 
   return (
     <div className="app">
       <div className="app__body">
       <Sidebar/>
-      <Chat />
+      <Chat messages={messages}/>
       </div>     
     </div>
   );
